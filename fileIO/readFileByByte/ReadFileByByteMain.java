fda577edac11e5d65bf75b977e8391618244b5fd
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-06 22:47:42 +0900

    [update] File main class updated

--- fileIO/readFileByByte/ReadFileByByteMain.java
+++ fileIO/readFileByByte/ReadFileByByteMain.java
@@ -8,9 +8,6 @@ public class ReadFileByByteMain {
             ReadFileByByte fileByByte = new ReadFileByByte("./arbitrary_file.txt");
 
             System.out.println(fileByByte.readOneByte());
-            System.out.println(fileByByte.readOneByte());
-            System.out.println(fileByByte.readOneByte());
-            System.out.println(fileByByte.readOneByte());
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
